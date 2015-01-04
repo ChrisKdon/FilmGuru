25e87290fdaf946e35c5bee161b31c40d21ca5bb
Chris Kellendonk <chriskellendonk@gmail.com>
2015-01-04 12:20:50 -0500

    Added test

---
+++ test/NeuralNetworkTest.java
@@ -0,0 +1,32 @@
+import com.monikle.neuro.FeedForwardNetwork;
+import com.monikle.neuro.NeuralNetwork;
+import com.monikle.neuro.TrainerConfiguration;
+import com.monikle.neuro.TrainingData;
+
+public class NeuralNetworkTest {
+	public static void main(String[] args) {
+		NeuralNetwork network = new FeedForwardNetwork(2, 5, 1, 0.1, 0.9);
+
+		TrainingData trainingData = new TrainingData();
+
+		trainingData.add(new double[] {0, 0}, new double[] {0});
+		trainingData.add(new double[] {0, 1}, new double[] {1});
+		trainingData.add(new double[] {1, 0}, new double[] {1});
+		trainingData.add(new double[] {1, 1}, new double[] {0});
+
+		trainingData.add(new double[] {0, 0}, new double[] {0});
+		trainingData.add(new double[] {0, 1}, new double[] {1});
+		trainingData.add(new double[] {1, 0}, new double[] {1});
+		trainingData.add(new double[] {1, 1}, new double[] {0});
+
+		TrainerConfiguration config = TrainerConfiguration.create(trainingData)
+				.setMaxEpochs(100)
+				.setAcceptableError(0.5)
+				.setValidationAmount(0.5)
+				.setShuffleTrainingData(false);
+
+		network.train(config);
+
+		System.out.println(network.run(1, 1));
+	}
+}
